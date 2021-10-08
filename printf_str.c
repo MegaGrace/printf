78d1ad82ec9b98c8e981c91a14a648b5760370f1
kyeimuda <kyeibernard01@gmail.com>
2021-10-08 04:04:15 -0700

    printf str

--- printf_str.c
+++ printf_str.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _strlen - Returns the lenght of a string.
  * @s: Type char pointer
  * Return: c.
  */
+
 int _strlen(char *s)
 {
 int c;
@@ -18,6 +20,7 @@ return (c);
  * @s: Type char pointer
  * Return: c
  */
+
 int _strlenc(const char *s)
 {
 int c;
